868534a318e566e06868a227838dc203f3e360bb
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2025-04-19 21:46:20 +0530

    Switch to HashRouter, set Vite base for GitHub Pages, add deploy scripts

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 
 
 )
